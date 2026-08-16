dea8c1b045c29fd5905640f255a7d3b9b595d82f
MTVirux <joaofraga96@outlook.com>
2026-08-16 17:41:18 +0100

    Mark the section headings with a collapse arrow

--- MarketTerror/GUI/Components/SectionHeading.cs
+++ MarketTerror/GUI/Components/SectionHeading.cs
@@ -5,7 +5,9 @@
 namespace MarketTerror.GUI.Components
 {
   using System;
+  using System.Numerics;
   using Dalamud.Bindings.ImGui;
+  using Dalamud.Interface;
 
   /// <summary>
   /// The heading of a market data section, which hides and shows its table when clicked.
@@ -32,6 +34,7 @@ namespace MarketTerror.GUI.Components
       }
 
       var clicked = ImGui.Selectable($"{text}##{id}", false);
+      var color = ImGui.GetColorU32(ImGuiCol.Text);
 
       if (collapsed)
       {
@@ -43,7 +46,34 @@ namespace MarketTerror.GUI.Components
         ImGui.SetMouseCursor(ImGuiMouseCursor.Hand);
       }
 
+      DrawArrow(collapsed, color);
+
       return clicked;
+    }
+
+    /// <summary>
+    /// Draws the arrow reporting the section's state, centred on the right edge of the heading.
+    /// </summary>
+    /// <param name="collapsed">True when the section's table is hidden.</param>
+    /// <param name="color">The colour of the heading text.</param>
+    private static void DrawArrow(bool collapsed, uint color)
+    {
+      var min = ImGui.GetItemRectMin();
+      var size = ImGui.GetItemRectSize();
+      var arrow = $"{(char)(collapsed ? FontAwesomeIcon.ChevronRight : FontAwesomeIcon.ChevronDown)}";
+
+      ImGui.PushFont(UiBuilder.IconFont);
+
+      var arrowSize = ImGui.CalcTextSize(arrow);
+
+      ImGui.GetWindowDrawList().AddText(
+        new Vector2(
+          min.X + size.X - arrowSize.X - ImGui.GetStyle().FramePadding.X,
+          min.Y + ((size.Y - arrowSize.Y) / 2.0f)),
+        color,
+        arrow);
+
+      ImGui.PopFont();
     }
   }
 }
